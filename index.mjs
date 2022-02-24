1e172152239d8745c3694b4c18881ef03e3698a3
HeHongbo <hehongbo@mail.com>
2022-02-24 15:20:46 +0800

    Refine log output

--- index.mjs
+++ index.mjs
@@ -3,9 +3,12 @@
 import express from "express";
 
 import {api} from "./lib/api/index.mjs";
+import {log} from "./lib/log.mjs";
 
 const app = express();
+const listenPort = 3000;
 
 app.use("/api", api);
 
-app.listen(3000, () => {});
+log(`Listening on port ${listenPort} ...`, "Info");
+app.listen(listenPort, () => {});
