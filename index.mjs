5997bb3711936de0981ccf421477b5b9a0d096d0
HeHongbo <hehongbo@mail.com>
2022-03-04 17:11:10 +0800

    Fix the bug where frontend assets will not be served properly when executed outside the project root

--- index.mjs
+++ index.mjs
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+import path from "path";
+import url from "url";
 import express from "express";
 
 import {api} from "./lib/api/index.mjs";
@@ -9,7 +11,9 @@ import {configStorage} from "./lib/config.mjs";
 
 const app = express();
 app.use("/api", api);
-app.use("/", express.static("front"));
+app.use("/", express.static(
+    path.join(path.dirname(url.fileURLToPath(import.meta.url)), "front"))
+);
 
 log(`Listening on port ${configStorage.listenPort} ...`, "Info");
 app.listen(configStorage.listenPort, () => {});
